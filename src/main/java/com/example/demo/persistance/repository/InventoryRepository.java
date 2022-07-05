394a838092bc26b030cdb0cd801dbd598144cafb
Shuki Basson <shuki.basson@gmail.com>
2022-07-05 12:03:47 +0300

    change pk

--- src/main/java/com/example/demo/persistance/repository/InventoryRepository.java
+++ src/main/java/com/example/demo/persistance/repository/InventoryRepository.java
@@ -1,12 +1,13 @@
 package com.example.demo.persistance.repository;
 
 import com.example.demo.persistance.entity.Inventory;
+import com.example.demo.persistance.entity.InventoryPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigInteger;
 import java.util.List;
 
-public interface InventoryRepository extends JpaRepository<Inventory, BigInteger> {
+public interface InventoryRepository extends JpaRepository<Inventory, InventoryPK> {
 
     List<Inventory> getInventoryById_ProductIdAndAndQuantityGreaterThan(BigInteger productId, int amount);
 }
